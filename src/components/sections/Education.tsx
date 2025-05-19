3522ad2a202a83dafb0052a03a47395c58ea24b0
Adrien Ruault <ruaultadrien@gmail.com>
2025-05-19 10:29:06 +0200

    Rephrase education statement

--- src/components/sections/Education.tsx
+++ src/components/sections/Education.tsx
@@ -8,15 +8,15 @@ export default function Education() {
       <SectionTitle title="Education" />
       <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
         <EmploymentHistoryItem
-          title="MSc in Computational Science and Engineering, EPFL - Ecole Polytechnique Fédérale de Lausanne"
+          title="MSc in Computational Science and Engineering, EPFL, Lausanne, Switzerland"
           date="September 2016 – July 2019 | Grade: 5.44/6"
         />
         <EmploymentHistoryItem
-          title="Erasmus, The University of Manchester (as part of my BSc at EPFL)"
+          title="Erasmus Exchange Year, The University of Manchester, Manchester, United Kingdom"
           date="September 2015 – July 2016"
         />
         <EmploymentHistoryItem
-          title="BSc in Materials Science and Engineering, EPFL - Ecole Polytechnique Fédérale de Lausanne"
+          title="BSc in Materials Science and Engineering, EPFL, Lausanne, Switzerland"
           date="September 2013 – July 2016 | Grade: 5.56/6"
         />
       </Box>
