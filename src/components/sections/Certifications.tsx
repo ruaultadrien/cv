e9ec9805bb63631f1094c3243014cfac6dc1e916
Adrien Ruault <ruaultadrien@gmail.com>
2025-05-23 00:53:13 +0200

    Implement pictures in certifications

--- src/components/sections/Certifications.tsx
+++ src/components/sections/Certifications.tsx
@@ -3,6 +3,10 @@ import SectionTitle from "../SectionTitle";
 import Grid from "@mui/material/Grid2";
 
 import CertificationItem from "./CertificationItem";
+import huggingFaceLogo from "../../assets/certifications/hugging_face.svg";
+import dataikuLogo from "../../assets/certifications/dataiku.png";
+import mqLearning from "../../assets/certifications/mq_learning.jpeg";
+import azureLogo from "../../assets/certifications/azure.svg";
 
 export default function Certifications() {
   return (
@@ -12,31 +16,41 @@ export default function Certifications() {
         <Grid size={{ xs: 12, sm: 6 }}>
           <CertificationItem
             title="Fundamentals of Agents"
-            date="Hugging Face, February 2025"
+            company="Hugging Face"
+            date="February 2025"
+            image={huggingFaceLogo}
           />
         </Grid>
         <Grid size={{ xs: 12, sm: 6 }}>
           <CertificationItem
             title="Azure Data Scientist Associate"
-            date="Microsoft, August 2024"
+            company="Microsoft"
+            date="August 2024"
+            image={azureLogo}
           />
         </Grid>
         <Grid size={{ xs: 12, sm: 6 }}>
           <CertificationItem
             title="Dataiku Core Designer"
-            date="Dataiku, April 2023"
+            company="Dataiku"
+            date="April 2023"
+            image={dataikuLogo}
           />
         </Grid>
         <Grid size={{ xs: 12, sm: 6 }}>
           <CertificationItem
             title="Foundational Strategic Thinking"
-            date="MQ Learning, July 2023"
+            company="MQ Learning"
+            date="July 2023"
+            image={mqLearning}
           />
         </Grid>
         <Grid size={{ xs: 12, sm: 6 }}>
           <CertificationItem
             title="Foundational Systemic People Management"
-            date="MQ Learning, February 2023"
+            company="MQ Learning"
+            date="February 2023"
+            image={mqLearning}
           />
         </Grid>
       </Grid>
