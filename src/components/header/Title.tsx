438c76582cb680a4e3e871782462fa28f30f5cfb
Adrien Ruault <ruaultadrien@gmail.com>
2025-09-16 16:29:42 +0200

    Change profile description and title

--- src/components/header/Title.tsx
+++ src/components/header/Title.tsx
@@ -2,7 +2,7 @@ export default function Title() {
   return (
     <div className="flex flex-col items-start justify-center">
       <h1 className="text-4xl font-bold">Adrien Ruault</h1>
-      <h2 className="text-3xl">Software Engineer</h2>
+      <h2 className="text-3xl">AI Software Engineer</h2>
     </div>
   );
 }
