840fba653ff9a78ca2af15a1edd7c9862d20d2cd
Adrien Ruault <ruaultadrien@gmail.com>
2025-12-18 16:31:12 +0100

    Apply biome formatting

--- src/components/sections/EmploymentHistory/CSEM.tsx
+++ src/components/sections/EmploymentHistory/CSEM.tsx
@@ -2,30 +2,30 @@ import csemLogo from "../../../assets/companies/csem.png";
 import EmploymentHistoryItem from "./EmploymentHistoryItem";
 
 export default function CSEM() {
-  return (
-    <EmploymentHistoryItem
-      title="Junior AI Research Engineer, CSEM, Neuchâtel, Switzerland"
-      date="February 2019 – July 2019 | 6 months"
-      image={
-        <img
-          src={csemLogo}
-          alt="CSEM logo"
-          className="p-1 object-contain rounded-lg bg-white"
-        />
-      }
-    >
-      <div className="flex flex-col gap-2">
-        <div>
-          <p>
-            <strong>AI Research -</strong> Researched the impact of
-            Reinforcement Learning for controlling the energy systems in
-            buildings, as part of my EPFL master thesis.
-          </p>
-          <p className="text-sm text-gray-600">
-            Python, Tensorflow, Reinforcement Learning
-          </p>
-        </div>
-      </div>
-    </EmploymentHistoryItem>
-  );
+	return (
+		<EmploymentHistoryItem
+			title="Junior AI Research Engineer, CSEM, Neuchâtel, Switzerland"
+			date="February 2019 – July 2019 | 6 months"
+			image={
+				<img
+					src={csemLogo}
+					alt="CSEM logo"
+					className="p-1 object-contain rounded-lg bg-white"
+				/>
+			}
+		>
+			<div className="flex flex-col gap-2">
+				<div>
+					<p>
+						<strong>AI Research -</strong> Researched the impact of
+						Reinforcement Learning for controlling the energy systems in
+						buildings, as part of my EPFL master thesis.
+					</p>
+					<p className="text-sm text-gray-600">
+						Python, Tensorflow, Reinforcement Learning
+					</p>
+				</div>
+			</div>
+		</EmploymentHistoryItem>
+	);
 }
