4bc7666e9beb9b3bfd17205354005e0d9233040c
Adrien Ruault <ruaultadrien@gmail.com>
2025-12-29 18:25:39 +0100

    Insall husky

--- src/components/sections/Profile.tsx
+++ src/components/sections/Profile.tsx
@@ -9,11 +9,11 @@ export default function Profile() {
 				sx={{ display: "flex", flexDirection: "column", gap: 1, ml: 1, mr: 1 }}
 			>
 				<Typography>
-					AI Engineer with 7+ years of experience delivering production-grade
-					AI systems end-to-end, from specification and model development to
-					deployment and monitoring. My strong cross-functional expertise in data
-					engineering, cloud platforms, MLOps, and frontend engineering enables
-					me to build scalable, real-world AI solutions.
+					AI Engineer with 7+ years of experience delivering production-grade AI
+					systems end-to-end, from specification and model development to
+					deployment and monitoring. My strong cross-functional expertise in
+					data engineering, cloud platforms, MLOps, and frontend engineering
+					enables me to build scalable, real-world AI solutions.
 				</Typography>
 				<Typography>Looking forward to connecting!</Typography>
 			</Box>
